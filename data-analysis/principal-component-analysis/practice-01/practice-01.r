b2b50a6888dea443a2339ae86e58d3e8d15e47bb
Sergio Garcia Prado <sergio@garciparedes.me>
2017-10-11 19:16:25 +0200

    Done with practice 1

--- data-analysis/principal-component-analysis/practice-01/practice-01.r
+++ data-analysis/principal-component-analysis/practice-01/practice-01.r
@@ -9,5 +9,26 @@
 #
 #
 
-library(gdata)
-DATA <- read.xls('./../../../datasets/olympic-2016.xls', header=TRUE)
+# library(gdata)
+suppressMessages(library(gdata))
+
+DATA <- read.xls('./../../../datasets/olympic-2016.xls')
+print(DATA)
+A <- as.matrix(DATA[,1:10])
+
+X <- t((t(A) - colMeans(A)) / apply(A, 2, sd))
+X_start = (t(X) %*% X ) / (dim(X)[1] - 1)
+print(X_start)
+
+X_lambda <- eigen(X_start)$values
+X_u <- eigen(X_start)$vector
+
+S <- X %*% X_u
+
+scores = as.data.frame(S)
+print(scores)
+
+library(ggplot2)
+ggplot(data = scores, aes(x=V1, y=V2, label=DATA[,12])) +
+  geom_point() +
+  geom_text()
