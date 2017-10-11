93d52413fdd1495d15690047d16fc8ec7c0b74f9
Sergio Garcia Prado <sergio@garciparedes.me>
2017-10-11 13:43:08 +0200

    Working on exercise

--- data-analysis/correspondence-analysis/practice-01/practice-01.r
+++ data-analysis/correspondence-analysis/practice-01/practice-01.r
@@ -11,6 +11,7 @@
 
 
 # 1.	Construir la matriz
+
 N <- t(matrix(
   c(20,20,20,
     40,10,40,
@@ -19,7 +20,9 @@ N <- t(matrix(
 ))
 print(N)
 
+
 # 2.	Construir las matrices F, Dr, Dc y las matrices de puntos fila y columna.
+
 F <- N / sum(N)
 print(F)
 
@@ -33,8 +36,9 @@ Pc <- solve(Dc) %*% t(F)
 print(Pr)
 print(Pc)
 
-t(Pr[1,]-Pr[2,]) %*% solve(Dc) %*% ((Pr[1,]-Pr[2,]))
+
 # 3. Calcular las distancias chi cuadrado entre los tres puntos fila
+
 d_chi_square <- function(x, y) t(x - y) %*% solve(Dc) %*% (x - y)
 D <- c(
   d_chi_square(Pr[1,],Pr[2,]),
@@ -42,6 +46,7 @@ D <- c(
   d_chi_square(Pr[2,],Pr[3,])
 )
 print(D)
+
 
 # 4. Calcular el valor del estadístico chi cuadrado
 
@@ -58,30 +63,44 @@ print(pval)
 # cuadrado al centro de gravedad de los puntos fila y comprobar que es el
 # estadístico chi cuadrado dividido por n
 
-# TODO
-# sum(Pr * d_chi_square_2(F,colSums(F))) / sum(N)
+Gc <- colSums(N) / sum(N)
+Gr <- rowSums(N) / sum(N)
+print(Gc)
+print(Gr)
+
+diag(t(t(Pr) - Gc) %*% solve(dc) %*% t(t(t(Pr)-Gc))) %*% Gr
+G <- (diag(t(t(Pr) - Gc) %*% solve(dc) %*% t(t(t(Pr)-Gc))) %*% Gr)[1,1]
+print(G)
+
 
 # 6. Construir la matriz a diagonalizar, diagonalizarla calculando los
 # autovalores y autovectores unitarios
 
 X <- t(F) %*% solve(Dr) %*% F %*% solve(Dc)
 print(X)
-X_eigen <- eigen(X)
+X_eigenvec <- eigen(X)$vectors
+X_eigenval <- eigen(X)$values
 
-print(X_eigen$values)
-print(X_eigen$vectors)
+print(X_eigenval)
+print(X_eigenvec)
 
-U <- (X_eigen$vectors / norm(X_eigen$vectors))
-print(U)
 # 7. Calcular los factores y las proyecciones de los puntos fila sobre los ejes
 
-proyecciones <- (solve(Dr) %*% F) %*% solve(Dc) %*% U
-print(proyecciones)
+
+fact <- solve(Dc) %*% X_eigenvec
+print(fact)
+
+Proyec_r <- Pr %*% fact
+print(Proyec_r)
+
+
 # 8. Calcular las distancias euclídeas entre las proyecciones y compararlas con
 # las del punto 3
+
 d_euclidean <- function(x, y) sum( (x / rowSums(F) - y / rowSums(F))^2)
 print(d_euclidean(F[,2], F[,3]))
-print(d_chi_square_2(F[,2], F[,3]))
+print(d_chi_square(F[,2], F[,3]))
+
 
 # 9. Calcular los factores y las proyecciones de los puntos columna sobre los
 # ejes
@@ -89,3 +108,12 @@ print(d_chi_square_2(F[,2], F[,3]))
 
 # 10. Efectuar una representación conjunta de las proyecciones de filas y
 # columnas
+
+Proyec_r_dataframe <- as.data.frame(Proyec_r[,2:3])
+rownames(Proyec_r_dataframe)<-c("J","ME","M")
+Proyec_r_dataframe
+
+library(ggplot2)
+ggplot(data = Proyec_r_dataframe, aes(x = V1, y = V2, label=rownames(Proyec_r_dataframe))) +
+  geom_text(colour = "red", size = 6) +
+  ggtitle("Proyección simultánea")
