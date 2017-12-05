346e372465468dfe6a86fe59d13259bc14a70d69
Sergio Garcia Prado <sergio@garciparedes.me>
2017-12-05 19:07:41 +0100

    Minor changes

--- data-analysis/cluster-analysis/iris-k-means.R
+++ data-analysis/cluster-analysis/iris-k-means.R
@@ -2,25 +2,18 @@
 # Project: r-examples (github.com/garciparedes/r-examples)
 # Author: Sergio García Prado (garciparedes.me)
 # Date: 2017-12
-# Title: Naive k-means implementation and proof over iris dataset 
+# Title: Naive k-means implementation 
 #
 
-
-data(iris)
-head(iris)
-
-k <- 3
-iris.use <- iris[,-c(5)]
-
+rm(list = ls())
 
 naiveKmeans <- function(data, k, tol = 0.1) {
     
   train <- function(data, k, tol) {
     ranges <- sapply(data, range)
-    coordinates <- (runif(dim(ranges)[2] * k, min=ranges[1,], max=ranges[2,]))
+    coordinates <- runif(dim(ranges)[2] * k, min=ranges[1,], max=ranges[2,])
     centers <- t(matrix(data = (coordinates), ncol = k))
     colnames(centers)<-colnames(data)
-    
     distance <-  as.matrix(dist(rbind(centers, data), upper=TRUE))[,1:k]
     dataClass <- apply(distance, 1,which.min)
     
@@ -52,6 +45,20 @@ naiveKmeans <- function(data, k, tol = 0.1) {
   
   
 }
+
+# Spherical and Normal Dataset
+n <- 100
+data <- data.frame(rbind(
+  cbind(rnorm(n, 3), rnorm(n,-20)),
+  cbind(rnorm(n, 9), rnorm(n,7)), 
+  cbind(rnorm(n, -3), rnorm(n,-2))
+))
+data.kmeans <- naiveKmeans(data, 3)
+
+data(iris)
+
+k <- 3
+iris.use <- iris[,-c(5)]
 
 iris.kmeans <- naiveKmeans(iris.use, 3)
 plot(iris[,1:4], col= as.numeric(iris[,5]) + 1)
