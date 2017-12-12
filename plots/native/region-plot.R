2accb95cca41c3c5aef420c2c11c93420c29801b
Sergio Garcia Prado <sergio@garciparedes.me>
2017-12-12 11:55:05 +0100

    Minor changes

--- plots/native/region-plot.R
+++ plots/native/region-plot.R
@@ -8,20 +8,22 @@
 
 rm(list = ls())
 
-x_lims <- c(-0.5, 1.5)
+x_lims <- c(-1.5, 1.5)
 y_lims <- c(-0.5, 1.5)
 
-plot(x_lims,y_lims, type='n')
 conditions <- function(x,y) {
   c1 <- (0 < y)
   c2 <- (y < 1)
-  c3 <- (0 < y/x)
-  c4 <- (y/x < 1)
+  c3 <- (0 < y-x)
+  c4 <- (y-x < 1)
   return(c1 & c2 & c3 & c4)
 }
 
 x <- seq(x_lims[1],x_lims[2],length=100)
 y <- seq(y_lims[1],y_lims[2],length=100)
-z <- expand.grid(x=x,y=y)
+z <- expand.grid(x = x, y = y)
 z <- z[conditions(z$x,z$y),]
+
+plot(x_lims,y_lims, type='n')
+grid(lty = 6) 
 points(z, col="red")
