a41cac15736ca310d87a464660ca4e650cede280
Sergio Garcia Prado <sergio@garciparedes.me>
2018-03-20 18:45:25 +0100

    Added template script

---
+++ template.R
@@ -0,0 +1,15 @@
+#
+# Title:
+#   R Script Template
+#
+# Description:
+#   A R script template.
+#
+# Authors:
+#   Sergio García Prado - garciparedes.me
+#
+# Date:
+#   2018 March
+#
+
+rm(list = ls())
