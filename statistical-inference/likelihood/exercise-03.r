f018d2a01fc37e57fbb8f1be86a616f679f1f252
Sergio Garcia Prado <sergio@garciparedes.me>
2018-11-11 11:08:13 +0100

    Done with exercises three and four

---
+++ statistical-inference/likelihood/exercise-03.r
@@ -0,0 +1,89 @@
+## Author: Sergio García Prado
+
+rm(list = ls())
+
+
+## a)
+####
+
+## Asyntotic distibution of (p1.hat, p2.hat) as parameters p1 of Bin(n, p1) and
+## p2 of Bin(n, p2) independent of each other.
+##
+## (p1.hat, p2.hat) ~ N_2( (p1, p2) , [p1 * (1 - p1) / n, 0;
+##                                     0, p2 * (1 - p2) / n] )
+##
+
+
+## b)
+####
+n1 <- 50
+y1 <- 35
+
+n2 <- 50
+y2 <- 40
+
+n <- c(n1, n2)
+y <- c(y1, y2)
+
+alpha <- 0.05
+
+LogLikelihood <- function(p, y, n) {
+  sum(y * log(p) + (n - y) * log(1 - p))
+}
+
+NegativeLogLikelihood <- function(...) {
+  - LogLikelihood(...)
+}
+
+opt <- optim(runif(2), NegativeLogLikelihood, y = y, n = n, hessian = TRUE,
+              lower = rep(10e-3, 2), upper = rep(1 - 10e-3, 2), method = "L-BFGS-B")
+
+(p.hat <- opt$par)
+# 0.69999745777599 0.799999683303738
+
+(p.hat.var <- solve(opt$hessian))
+# 4.199970e-03	-2.387315e-14
+# -2.387315e-14	3.199878e-03
+
+g.transform <- c(-2, 1)
+
+(g.hat <- (g.transform %*% p.hat)[1])
+# -0.59999999367853
+
+(g.hat.var <- (g.transform %*% p.hat.var %*% g.transform)[1])
+# 0.0199995879878169
+
+## Wald's Confidence Interval at (1 - alpha)% level.
+g.hat + c(-1, 1) * qnorm(1 - alpha / 2) * sqrt(g.hat.var)
+# -0.87717790348746 -0.3228220838696
+
+
+## b)
+####
+g.zero <- 0
+
+(W <- (g.hat - g.zero) ^ 2 / g.hat.var)
+# 18.0003059668258
+
+(W.pvalue <- 1 - pchisq(W, df = 1))
+# 2.20869627943765e-05
+
+## b)
+####
+
+LogLikelihoodHZero <- function(pp, ...) {
+  LogLikelihood(c(2 * pp, pp), ...)
+}
+
+NegativeLogLikelihoodHZero <- function(...) {
+  - LogLikelihoodHZero(...)
+}
+
+opt.hzero <- optim(runif(1), NegativeLogLikelihoodHZero, y = y, n = n,
+                   lower = 10e-3, upper = 0.5 - 10e-3, method = "L-BFGS-B" )
+
+(LRT <- 2 * (LogLikelihood(p.hat, y, n) - LogLikelihoodHZero(opt.hzero$par, y, n)))
+# 36.089146035026
+
+(LRT.pvalue <- 1 - pchisq(LRT, df = 1))
+# 1.88493831654313e-09
---
+++ statistical-inference/likelihood/exercise-04.r
@@ -0,0 +1,88 @@
+## Author: Sergio García Prado
+
+rm(list = ls())
+
+
+## a)
+####
+
+## Asyntotic distibution of (lambda1.hat, lambda2.hat) as parameters lambda1 of Pois(lambda1) and
+## lambda2 of Pois(lambda2) independent of each other.
+##
+## (lambda1.hat, lambda2.hat) ~ N_2( (lambda1, lambda2) , [lambda1 / n, 0;
+##                                                         0, lambda2 / n] )
+##
+
+
+## b)
+####
+n1 <- 50
+y1 <- 75
+
+n2 <- 50
+y2 <- 100
+
+n <- c(n1, n2)
+y <- c(y1, y2)
+
+alpha <- 0.05
+
+LogLikelihood <- function(lambda, y, n) {
+  sum(y * log(lambda) - n * lambda)
+}
+
+NegativeLogLikelihood <- function(...) {
+  - LogLikelihood(...)
+}
+
+opt <- optim(runif(2), NegativeLogLikelihood, y = y, n = n, hessian = TRUE)
+
+(lambda.hat <- opt$par)
+#
+
+(lambda.hat.var <- solve(opt$hessian))
+#
+#
+
+g.transform <- c(-2, 1)
+
+(g.hat <- (g.transform %*% lambda.hat)[1])
+#
+
+(g.hat.var <- (g.transform %*% lambda.hat.var %*% g.transform)[1])
+#
+
+## Wald's Confidence Interval at (1 - alpha)% level.
+g.hat + c(-1, 1) * qnorm(1 - alpha / 2) * sqrt(g.hat.var)
+#
+
+
+## b)
+####
+g.zero <- 0
+
+(W <- (g.hat - g.zero) ^ 2 / g.hat.var)
+#
+
+(W.pvalue <- 1 - pchisq(W, df = 1))
+#
+
+## b)
+####
+
+LogLikelihoodHZero <- function(ll, ...) {
+  LogLikelihood(c(2 * ll, ll), ...)
+}
+
+NegativeLogLikelihoodHZero <- function(...) {
+  - LogLikelihoodHZero(...)
+}
+
+opt.hzero <- optim(runif(1), NegativeLogLikelihoodHZero, y = y, n = n,
+                   lower = 10e-3, upper = 10e3, method = "Brent")
+
+(LRT <- 2 * (LogLikelihood(lambda.hat, y, n) - LogLikelihoodHZero(opt.hzero$par, y, n)))
+#
+
+(LRT.pvalue <- 1 - pchisq(LRT, df = 1))
+#
