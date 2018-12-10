99c4dd1fa6c502d35bb7bb491daafad2faa0f897
Sergio Garcia Prado <sergio@garciparedes.me>
2018-12-10 20:10:03 +0100

    Fixed pvalues with monte carlo simulations

--- statistical-inference/goodness-of-fit/exercise-15.r
+++ statistical-inference/goodness-of-fit/exercise-15.r
@@ -6,7 +6,20 @@ rm(list = ls())
 
 observed <- c(1.0, 2.3, 4.2, 7.1, 10.4)
 
+
+(k <- length(observed))
+# 5
+
+(lambda.hat <- 1 / mean(observed))
+# 0.2
+
 test <- ks.test(observed, pexp, rate = 1 / mean(observed))
 
-(pvalue <- test$p.value)
-# 0.986386731782078
+(D <- test$statistic[[1]])
+# 0.181269246922018
+
+(criticvalue <- quantile(replicate(1000, ks.test(rexp(k, lambda.hat), pexp, rate = lambda.hat)$statistic), 0.95))
+# 95%: 0.56811734613071
+
+D > criticvalue
+# FALSE (H0 no refused)
--- statistical-inference/goodness-of-fit/exercise-20.r
+++ statistical-inference/goodness-of-fit/exercise-20.r
@@ -6,10 +6,19 @@ rm(list = ls())
 
 observed <- c(1.5, 2.3, 4.2, 7.1, 10.4, 8.4, 9.3, 6.5, 2.5, 4.6)
 
-test <- ks.test(observed, pexp, rate = 1 / mean(observed))
+(k <- length(observed))
+# 10
+
+(lambda.hat <-1 / mean(observed))
+# 0.176056338028169
+
+test <- ks.test(observed, pexp, rate = lambda.hat)
 
 (D <- test$statistic[[1]])
 # 0.232976217289659
 
-(pvalue <- test$p.value)
-# 0.572663346985868
+(criticvalue <- quantile(replicate(1000, ks.test(rexp(k, lambda.hat), pexp, rate = lambda.hat)$statistic), 0.95))
+# 0.4109577865723
+
+D > criticvalue
+# FALSE (H0 no refused)
