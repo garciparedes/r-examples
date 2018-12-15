776feb17d0596cb5a7195282c63521eef98021bb
Sergio Garcia Prado <sergio@garciparedes.me>
2018-12-15 19:34:27 +0100

    Done with exercise 10 #8

---
+++ stochastic-processes/durret-4-10.R
@@ -0,0 +1,20 @@
+## Author: Sergio García Prado
+## Title: Durret - Exercise 4.10
+
+rm(list = ls())
+
+Stationary <- function(l, m) {
+  p0 <- mul(m) / (mul(m) + l[1] * m[2] * m[3] + m[1] * l[2] * m[3] + m[1] * m[2] * l[3])
+  stationary <- c(1, l / p) * p0
+  return(stationary)
+}
+
+
+QGenerator <- function(l, m) {
+  Q <- matrix(c(-sum(l), l[1],   l[2], l[3],
+                   m[1], -m[1],     0,    0,
+                   m[2],     0, -m[2],    0,
+                   m[3],     0,     0, -m[3],),
+              4, 4, byrow = TRUE)
+  return(Q)
+}
