57f6383946e6802a5521be12c6e7667f4b9219eb
Sergio Garcia Prado <sergio@garciparedes.me>
2018-12-15 15:37:36 +0100

    Added output as comments

--- stochastic-processes/durret-4-01.R
+++ stochastic-processes/durret-4-01.R
@@ -9,9 +9,14 @@ Q <- matrix(c(-4,  2, 2,
             3, 3, byrow = TRUE)
 
 
-A <- cbind(Q[,1:(nrow(Q) - 1) ], rep(1, nrow(Q)))
+(A <- cbind(Q[,1:(nrow(Q) - 1) ], rep(1, nrow(Q))))
+# -4	 2	1
+#  3	-4	1
+#  5	 0	1
 
 (stationary <- solve(A)[nrow(A), ])
+# 0.5 0.25 0.25
 
 
 (trips.boston.atlanta <- 12 * stationary[2] * Q[2, 1])
+# 9
