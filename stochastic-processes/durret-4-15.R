58e61f5234d491b88f570ec495a5f57e4d080eae
Sergio Garcia Prado <sergio@garciparedes.me>
2018-12-16 11:04:53 +0100

    Done with exercise 16 #8

--- stochastic-processes/durret-4-15.R
+++ stochastic-processes/durret-4-15.R
@@ -9,8 +9,13 @@ Q <- matrix(c(  -1,      1,    0,     0,
                  0,      0, 3/20, -3/20),
             4, 4, byrow = TRUE)
 
-
 A <- cbind(Q[,1:(nrow(Q) - 1) ], rep(1, nrow(Q)))
 
 (stationary <- solve(A)[nrow(A), ])
 # 0.00112654900488171 0.0225309800976342 0.225309800976342 0.751032669921142
+
+(reparing.ratio <- stationary[1] + stationary[2])
+# 0.023657529102516
+
+(mean.working <- sum(0:3 * stationary))
+# 2.72624859181374
