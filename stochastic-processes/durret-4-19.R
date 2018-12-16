96a69344fc55101940abc8ad1854ea2d28beafa3
Sergio Garcia Prado <sergio@garciparedes.me>
2018-12-16 12:01:19 +0100

    Done with exercise 19 #8

---
+++ stochastic-processes/durret-4-19.R
@@ -0,0 +1,25 @@
+## Author: Sergio García Prado
+## Title: Durret - Exercise 4.16
+
+rm(list = ls())
+
+l <- 5
+mu <- 2
+
+(Q <- matrix(c(-l,        l,             0,              0,          0,
+               mu, -(l + mu),             l,             0,          0,
+                0,    2 * mu, -(l + 2 * mu),             l,          0,
+                0,         0,        2 * mu, -(l + 2 * mu),          l,
+                0,         0,             0,        2 * mu, -(2 * mu)),
+             5, 5, byrow = TRUE))
+# -5	 5	 0	 0	 0
+#  2	-7	 5	 0	 0
+#  0	 4	-9	 5	 0
+#  0	 0	 4	-9	 5
+#  0	 0	 0	 4	-4
+
+## Should be solved via detailed balance equations...
+A <- cbind(Q[,1:(nrow(Q) - 1) ], rep(1, nrow(Q)))
+(stationary <- solve(A)[nrow(A), ])
+# 0.0648758236188546 0.162189559047136 0.20273694880892
+# 0.253421186011151  0.316776482513938
