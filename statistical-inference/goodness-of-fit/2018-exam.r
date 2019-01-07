deb3b6e1d9c9c8cecb5663f36ddae209e51aa251
Sergio Garcia Prado <sergio@garciparedes.me>
2019-01-07 19:59:18 +0100

    Fixed typo

--- statistical-inference/goodness-of-fit/2018-exam.r
+++ statistical-inference/goodness-of-fit/2018-exam.r
@@ -8,7 +8,7 @@ rm(list = ls())
 
 ### Ejercicio 1 - Apartado A
 
-observed1 <- c(0.87, 0.20, 0.99, 0.40, 0.39, 0.72, 0.33, 0.88, 0.22, 0.45, 
+observed1 <- c(0.87, 0.20, 0.99, 0.40, 0.39, 0.72, 0.33, 0.88, 0.22, 0.45,
                0.01, 0.37, 0.59, 0.80, 0.93, 0.54, 0.66, 0.49, 0.73, 0.68)
 
 (n1 <- length(observed1))
@@ -16,7 +16,7 @@ observed1 <- c(0.87, 0.20, 0.99, 0.40, 0.39, 0.72, 0.33, 0.88, 0.22, 0.45,
 
 ## Función de distribución bajo hipótesis nula.
 CummulativeH0 <- function(x) {
-  ifelse(x < 0, 0, 
+  ifelse(x < 0, 0,
   ifelse(1 < x, 1,
   (1 / 3) * x ^ 4 * (5 - 2 * x)))
 }
@@ -30,8 +30,8 @@ test1 <- ks.test(observed1, CummulativeH0)
 (pvalue1 <- test1$p.value)
 # 0.001244618
 
-## Nos vemos obligados a rechazar la hipótesis de que los datos observados 
-## pertenezcan a la distribución definida en la hipótesis nula. 
+## Nos vemos obligados a rechazar la hipótesis de que los datos observados
+## pertenezcan a la distribución definida en la hipótesis nula.
 
 
 ### Ejercicio 1 - Apartado B
@@ -39,7 +39,7 @@ test1 <- ks.test(observed1, CummulativeH0)
 alpha <- 0.05
 
 ## Para simular la potencia calculamos el promedio sobre la variable indicador
-## que toma el valor 1 cuando rechazamos la hipótesis nula, y 0 en cuando la 
+## que toma el valor 1 cuando rechazamos la hipótesis nula, y 0 en cuando la
 ## aceptamos.
 
 (power1 <- mean(replicate(1000, ks.test(runif(n1), CummulativeH0)$p.value < alpha)))
@@ -68,7 +68,7 @@ observed2 <- c(2.27,2.28,0.05,0.56,0.59,1.21,0.46,1.74,1.66,0.66,1.94,0.33,
 (observed2.sd <- sd(observed2))
 # 1.609788
 
-## Generamos una muestra bootstrap paramétrica asumiendo una distribución 
+## Generamos una muestra bootstrap paramétrica asumiendo una distribución
 ## normal en los datos.
 bootstrap.sample <- matrix(rnorm(n = n2 * nsims2, mean = observed2.mean, sd = observed2.sd),
                            nsims2, n2)
@@ -78,7 +78,7 @@ bootstrap.mean <- rowMeans(bootstrap.sample)
 ## Utilizamos el mismo alpha que en el ejercicio anterior (alpha = 0.05)
 
 quantile(bootstrap.mean, c(alpha / 2, 1 - alpha / 2))
-# 2.5%     97.5% 
+# 2.5%     97.5%
 # 1.054534 1.886941
 
 
@@ -91,42 +91,42 @@ observed3 <- c(20, 18, 8, 14)
 # 4
 
 (n3 <- sum(observed3))
-# 60 
+# 60
 
 ### Ejercicio 3 - Apartado A
 
 
-PoissonExcepted <- function(n, k, lambda) {
+PoissonExpected <- function(n, k, lambda) {
   ## Método 1
-  
-  # n * c(dpois(0:(k - 2), lambda = lambda), 
+
+  # n * c(dpois(0:(k - 2), lambda = lambda),
   #       ppois(k - 2, lambda = lambda, lower.tail = FALSE))
-  
+
   ## Método 2:
-  
+
   Probability <- function(x) {
     lambda ^ x * exp(- lambda) / factorial(x)
   }
-  
+
   n * c(Probability(0:(k - 2)), 1 - sum(Probability(0:(k - 2))))
 }
 
-NegativeBinomialExcepted <- function(n, k, size, prob) {
+NegativeBinomialExpected <- function(n, k, size, prob) {
   ## Método 1
-  
-  # n * c(dnbinom(0:(k - 2), size = size, prob = prob), 
+
+  # n * c(dnbinom(0:(k - 2), size = size, prob = prob),
   #       pnbinom(k - 2, size = size, prob = prob, lower.tail = FALSE))
-  
+
   ## Método 2:
-  
+
   Probability <- function(x) {
     (x + 1) * prob ^ 2 * (1 - prob) ^ x
   }
-  
+
   n * c(Probability(0:(k - 2)), 1 - sum(Probability(0:(k - 2))))
 }
 
-(expected3 <- PoissonExcepted(n3, k3, 1.2))
+(expected3 <- PoissonExpected(n3, k3, 1.2))
 # 18.071653 21.685983 13.011590  7.230774
 
 (Q3 <- sum((observed3 - expected3) ^ 2 / expected3))
@@ -135,16 +135,16 @@ NegativeBinomialExcepted <- function(n, k, size, prob) {
 (pvalue3 <- 1 - pchisq(Q3, k3 - 1))
 # 0.02799433
 
-## Nos vemos obligados a rechazar la hipótesis nula (los datos siguen una 
+## Nos vemos obligados a rechazar la hipótesis nula (los datos siguen una
 ## distribución de Poisson(1.2)) en favor de la alternativa (no pertenecen
 ## a dicha distribución).
 
 ### Ejercicio 3 - Apartado B
 
-(null3 <- PoissonExcepted(n3, k3, 1.2))
+(null3 <- PoissonExpected(n3, k3, 1.2))
 # 18.071653 21.685983 13.011590  7.230774
 
-(alternative3 <- NegativeBinomialExcepted(n3, k3, 2, 2 / 3))
+(alternative3 <- NegativeBinomialExpected(n3, k3, 2, 2 / 3))
 # 26.666667 17.777778  8.888889  6.666667
 
 (Q3b <- sum((alternative3 - null3) ^ 2 / null3))
@@ -156,8 +156,8 @@ NegativeBinomialExcepted <- function(n, k, size, prob) {
 ### Ejercicio 3 - Apartado C
 
 fn <- function(n) {
-  null <- PoissonExcepted(n, k3, 1.2)
-  alternative <- NegativeBinomialExcepted(n, k3, 2, 2 / 3)
+  null <- PoissonExpected(n, k3, 1.2)
+  alternative <- NegativeBinomialExpected(n, k3, 2, 2 / 3)
   Q <- sum((alternative - null) ^ 2 / null)
   power <- 1 - pchisq(qchisq(1 - alpha, df = k3 - 1), df = k3 - 1, ncp = Q)
   return(power)
@@ -170,7 +170,7 @@ ceiling(n.exact3)
 # 139
 
 
-## Necesitamos un tamaño muestral de 139 observaciones para poder obtener una 
-## potencia == 0.9 en el contraste acerca de si los datos siguen una 
+## Necesitamos un tamaño muestral de 139 observaciones para poder obtener una
+## potencia == 0.9 en el contraste acerca de si los datos siguen una
 ## distribución de Poisson(1.2) cuando realmente pertenecen a una distribución
 ## BinomialNegativa(2, 2/3)
