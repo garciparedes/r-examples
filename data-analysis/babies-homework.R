0e0e18439ad859f97eafb27c5e7f77d33da28bc6
Sergio Garcia Prado <sergio@garciparedes.me>
2019-05-21 12:26:27 +0200

    Added percentage ranking

--- data-analysis/babies-homework.R
+++ data-analysis/babies-homework.R
@@ -24,7 +24,6 @@ babies$smoke <- factor(babies$smoke, labels = c("non smokers", "smokers"))
 #   mother's pre-pregnancy height, weight, parity or age. Is it true? What is 
 #   the most important factor? What is the joint effect of all covariates?
 
-colnames(babies)
 
 ## Modelling
 
@@ -37,33 +36,47 @@ models$no.height = update(models$full, bwt ~ . - height)
 models$no.weight = update(models$full, bwt ~ . - weight)
 models$no.smoke = update(models$full, bwt ~ . - smoke)
 
+
 ## Analyzing
 
 ### Full model
 
 summary(models$full)
 
-## Ranking
 
-(rank <- 
+### R squared
+
+(r.squared <- 
   sort(sapply(models, function(x) { 
     summary(x)$r.squared 
   })))
 
-## Plotting
-
 par(mai=c(0.5,1.5,0.5,0.5))
-bp <- barplot(rank, horiz = TRUE, las=1, main = "R Squared by model")
-text(rank, bp, round(rank, 5), pos=2)
+bp <- barplot(r.squared, horiz = TRUE, las=1, main = "R Squared by model")
+text(r.squared, bp, round(r.squared, 5), pos=2)
+
+
+### Ranks
+
+rank <- rev(sapply(r.squared[-length(r.squared)], function(x) {
+  (r.squared[length(r.squared)] - x) / x * 100
+}))
+names(rank) <- sapply(strsplit(names(rank), split='\\.'), function(x){
+  x[2]
+})
+
+par(mai=c(0.5,1,0.5,0.5))
+bp <- barplot(rank, horiz = TRUE, las=1, main = "R Squared percentage difference by variable")
+
 
 ## Results
 
-### Smooking is the more most significant factor?
- rank['no.smoke'] == min(rank)
+### Smooking is the  most significant factor?
+rank['smoke'] == max(rank)
 #### FALSE
 
 ### Most important variable
-rank[rank == min(rank)]
+rank[rank == max(rank)]
 #### Gestation
 
 ### Joint effect of all covariates
